fdac6c6774df40bb06f98679c37fe4c12ca92b0b
jonarroh <jonarrodi99@gmail.com>
2023-04-25 17:06:56 -0600

    ✨ feat (trpc) Added private procedure

---
+++ src/components/UI/LoadingSpinner.tsx
@@ -0,0 +1,7 @@
+import { Spinner } from "flowbite-react";
+
+function LoadingSpinner() {
+  return <Spinner color="pink" aria-label="Pink spinner example" />;
+}
+
+export default LoadingSpinner;
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,8 +1,13 @@
 import { type User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
+import { z } from "zod";
 
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 const filterUserForClient = (user: User) => {
   return {
@@ -38,4 +43,22 @@ export const postRouter = createTRPCRouter({
       };
     });
   }),
+
+  create: privateProcedure
+    .input(
+      z.object({
+        content: z.string().min(1).max(280),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.currentUser.id;
+      const post = await ctx.prisma.post.create({
+        data: {
+          content: input.content,
+          authorId,
+          autorName: ctx.currentUser.firstName ?? "Unknown",
+        },
+      });
+      return post;
+    }),
 });
